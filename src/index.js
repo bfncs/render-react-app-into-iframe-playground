53d7297c3732ee17e8bcd45dcf499b6de8870895
Marc Löhe <loehe@sipgate.de>
2017-05-10 10:53:17 +0200

    Render react app into iframe

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const iframe = document.createElement('iframe');
+document.body.appendChild(iframe);
+
+const iframeDocument = iframe.contentDocument;
+const root = iframeDocument.createElement('div');
+iframeDocument.body.appendChild(root);
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App />,
+	root,
 );
